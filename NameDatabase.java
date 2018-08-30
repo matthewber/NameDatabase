69e5de52f72e93229a05cdb64f9d0ed27bcf9ca2
Matthew Berntsen <matthewberntsen@Matthews-MacBook-Air-2.local>
2018-08-29 21:46:55 -0700

    why is a message not optimal?

--- NameDatabase.java
+++ NameDatabase.java
@@ -1,6 +1,33 @@
+import java.io.*;
+
 public class NameDatabase{
 
+  private SkipList database;
 
+  public NameDatabase(){
+    this.database = new SkipList();
+  }
+
+  public void add(String fileName){
+    BufferedReader reader = null;
+    try{
+      reader = new BufferedReader(new FileReader(fileName));
+      String currLine = null;
+      while((currLine = reader.readLine())!= null){
+        database.insert(currLine);
+      }
+    }catch(FileNotFoundException e){
+      e.printStackTrace();
+    }catch(IOException e){
+      e.printStackTrace();
+    }
+
+  }
+
+  public static void main(String[] args){
+    NameDatabase nameData = new NameDatabase();
+    //nameData.add(args[1]);
+  }
 
 
 }
